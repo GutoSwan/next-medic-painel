163de852c8d7de6a3f39c6d6088bddb250d8cecb
GutoSwan <103047445+GutoSwan@users.noreply.github.com>
2024-05-13 09:47:07 -0300

    modal structure

--- src/components/modal/page_patient.tsx
+++ src/components/modal/page_patient.tsx
@@ -4,12 +4,13 @@ const Modal: React.FC = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [patient, setPatient] = useState("")
   const [date, setDate] = useState("")
-  const [cpf, setcpf] = useState("")
-  const [cep, setcep] = useState("")
-  const [motherName, setmotherName] = useState("")
-  const [sus, setsus] = useState("")
-  const [maritalStatus, setmaritalStatus] = useState("")
-  const [phone, setphone] = useState("")
+  const [cpf, setCpf] = useState("")
+  const [rg, setRg] = useState("")
+  const [cns, setCns] = useState("")
+  const [cep, setCep] = useState("")
+  const [motherName, setMotherName] = useState("")
+  const [maritalStatus, setMaritalStatus] = useState("")
+  const [phone, setPhone] = useState("")
 
   const openModal = () => {
     setIsModalVisible(true)
@@ -26,21 +27,22 @@ const Modal: React.FC = () => {
       patient,
       date,
       cpf,
+      rg,
+      cns,
       cep,
       motherName,
-      sus,
       maritalStatus,
       phone,
     })
 
     setPatient("")
     setDate("")
-    setcpf("")
-    setcep("")
-    setmotherName("")
-    setsus("")
-    setmaritalStatus("")
-    setphone("")
+    setCpf("")
+    setCns("")
+    setCep("")
+    setMotherName("")
+    setMaritalStatus("")
+    setPhone("")
     closeModal()
   }
 
@@ -57,17 +59,27 @@ const Modal: React.FC = () => {
               onSubmit={handleSubmit}
               className="flex flex-wrap justify-between"
             >
-              <div className="flex flex-col md:w-1/1">
+              <div className="flex flex-col md:w-1/2">
                 <label htmlFor="patient">Nome:</label>
                 <input
                   type="text"
                   id="patient"
                   value={patient}
                   onChange={(e) => setPatient(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1  w-[223px]"
+                  className="border-black border-solid border-2 rounded px-1 py-1  w-[350px]"
                 />
               </div>
-              <div className="flex flex-col w-full md:w-1/3">
+              <div className="flex flex-col w-full md:w-1/2">
+                <label htmlFor="date">Nome da Mãe:</label>
+                <input
+                  type="text"
+                  id="date"
+                  value={motherName}
+                  onChange={(e) => setMotherName(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[350px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/4">
                 <label htmlFor="date">Dt. Nascimento</label>
                 <input
                   type="text"
@@ -83,68 +95,68 @@ const Modal: React.FC = () => {
                   type="text"
                   id="duration"
                   value={cpf}
-                  onChange={(e) => setcpf(e.target.value)}
+                  onChange={(e) => setCpf(e.target.value)}
                   className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
                 />
               </div>
-              <div className="flex flex-col md:w-1/3">
+              <div className="flex flex-col w-full md:w-1/4">
+                <label htmlFor="cpf">RG:</label>
+                <input
+                  type="text"
+                  id="duration"
+                  value={rg}
+                  onChange={(e) => setRg(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/4">
+                <label htmlFor="cpf">CNS:</label>
+                <input
+                  type="text"
+                  id="duration"
+                  value={cns}
+                  onChange={(e) => setCns(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                />
+                <div className="flex flex-col w-full md:w-1/2">
+                  <label htmlFor="duration">Telefone:</label>
+                  <input
+                    type="text"
+                    id="duration"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col md:w-1/2">
                 <label htmlFor="patient">CEP:</label>
                 <input
                   type="text"
                   id="patient"
                   value={cep}
-                  onChange={(e) => setcep(e.target.value)}
+                  onChange={(e) => setCep(e.target.value)}
                   className="border-black border-solid border-2 rounded px-1 py-1  w-[223px]"
                 />
               </div>
-              <div className="flex flex-col w-full md:w-1/3">
-                <label htmlFor="date">Nome da Mãe:</label>
-                <input
-                  type="text"
-                  id="date"
-                  value={motherName}
-                  onChange={(e) => setmotherName(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
-                />
-              </div>
-              <div className="flex flex-col w-full md:w-1/4">
-                <label htmlFor="duration">Cartão SUS:</label>
-                <input
-                  type="text"
-                  id="duration"
-                  value={sus}
-                  onChange={(e) => setsus(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
-                />
-              </div>
-              <div className="flex flex-col w-full md:w-1/5">
+              <div className="flex flex-col w-full md:w-1/2">
                 <label htmlFor="duration">Estado civil:</label>
                 <input
                   type="text"
                   id="duration"
                   value={maritalStatus}
-                  onChange={(e) => setmaritalStatus(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
-                />
-              </div>
-              <div className="flex flex-col w-full md:w-1/4">
-                <label htmlFor="duration">Telefone:</label>
-                <input
-                  type="text"
-                  id="duration"
-                  value={phone}
-                  onChange={(e) => setphone(e.target.value)}
+                  onChange={(e) => setMaritalStatus(e.target.value)}
                   className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
                 />
               </div>
 
               <button
-                className="flex flex-col md:w-1/5"
+                className="flex flex-col md:w-1/1 bg-gradient-to-b from-cyan-600 to-blue-800"
                 type="button"
                 onClick={closeModal}
               >
                 <br />
-                Fechar
+                Adicionar
               </button>
             </form>
           </div>
--- src/components/modal/page_provider.tsx
+++ src/components/modal/page_provider.tsx
@@ -7,6 +7,15 @@ const Modal: React.FC = () => {
   const [cpf, setCpf] = useState("")
   const [cep, setCep] = useState("")
   const [acting, setActing] = useState("")
+  const [rg, setRg] = useState("")
+  const [cns, setCns] = useState("")
+  const [crm, setCrm] = useState("")
+  const [cbo, setCbo] = useState("")
+  const [mothername, setMotherName] = useState("")
+  const [maritalstatus, setMaritalStatus] = useState("")
+  const [specialty, setSpecialty] = useState("")
+  const [activity, setActivity] = useState("")
+  const [phone, setPhone] = useState("")
 
   const openModal = () => {
     setIsModalVisible(true)
@@ -21,17 +30,34 @@ const Modal: React.FC = () => {
 
     console.log({
       provider,
+      phone,
       date,
       cpf,
+      cns,
       cep,
       acting,
+      rg,
+      crm,
+      cbo,
+      mothername,
+      maritalstatus,
+      specialty,
+      activity,
     })
 
     setProvider("")
     setDate("")
+    setRg("")
     setCpf("")
     setCep("")
-    setActing("")
+    setCns("")
+    setCrm("")
+    setCbo("")
+    setMotherName("")
+    setMaritalStatus("")
+    setSpecialty("")
+    setActivity("")
+    setPhone("")
     closeModal()
   }
 
@@ -40,7 +66,7 @@ const Modal: React.FC = () => {
       <button onClick={openModal}>Adicionar prestador</button>
       {isModalVisible ? (
         <div className="absolute min-h-dvh min-w-full top-0 left-0 flex items-center justify-center">
-          <div className="bg-white rounded w-[766px] h-[250px]">
+          <div className="bg-white rounded w-[766px] h-[380px]">
             <h1 className="text-center text-2xl font-bold mb-4 mt-4 text-blue-500">
               Prestador
             </h1>
@@ -48,64 +74,153 @@ const Modal: React.FC = () => {
               onSubmit={handleSubmit}
               className="flex flex-wrap justify-between"
             >
-              <div className="flex flex-col md:w-1/1">
+              <div className="flex flex-col md:w-1/2">
                 <label htmlFor="provider">Nome:</label>
                 <input
                   type="text"
                   id="provider"
                   value={provider}
                   onChange={(e) => setProvider(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1  w-[223px]"
+                  className="border-black border-solid border-2 rounded px-1 py-1  w-[350px]"
                 />
               </div>
-              <div className="flex flex-col w-full md:w-1/3">
+              <div className="flex flex-col md:w-1/2">
+                <label htmlFor="mothername">Nome da Mãe:</label>
+                <input
+                  type="text"
+                  id="provider"
+                  value={mothername}
+                  onChange={(e) => setMotherName(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1  w-[350px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/5">
                 <label htmlFor="date">Dt. Nascimento</label>
                 <input
                   type="text"
                   id="date"
                   value={date}
                   onChange={(e) => setDate(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[171px]"
                 />
               </div>
-              <div className="flex flex-col w-full md:w-1/4">
+              <div className="flex flex-col w-full md:w-1/5">
                 <label htmlFor="cpf">CPF:</label>
                 <input
                   type="text"
-                  id="duration"
+                  id="cpf"
                   value={cpf}
                   onChange={(e) => setCpf(e.target.value)}
-                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[171px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/5">
+                <label htmlFor="rg">RG:</label>
+                <input
+                  type="text"
+                  id="rg"
+                  value={rg}
+                  onChange={(e) => setRg(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[171px]"
+                />
+              </div>
+              <div className="flex flex-col md:w-1/4">
+                <label htmlFor="cns">CNS:</label>
+                <input
+                  type="text"
+                  id="cns"
+                  value={cns}
+                  onChange={(e) => setCns(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1  w-[171px]"
                 />
               </div>
               <div className="flex flex-col md:w-1/3">
-                <label htmlFor="patient">CEP:</label>
+                <label htmlFor="phone">Telefone:</label>
                 <input
                   type="text"
-                  id="patient"
-                  value={cep}
-                  onChange={(e) => setCep(e.target.value)}
+                  id="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
                   className="border-black border-solid border-2 rounded px-1 py-1  w-[223px]"
                 />
               </div>
               <div className="flex flex-col w-full md:w-1/3">
-                <label htmlFor="date">Nome da Mãe:</label>
+                <label htmlFor="cep">CEP:</label>
                 <input
                   type="text"
-                  id="date"
-                  value={acting}
-                  onChange={(e) => setActing(e.target.value)}
+                  id="cep"
+                  value={cep}
+                  onChange={(e) => setCep(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[195px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/3">
+                <label htmlFor="maritalstatus">Estado Civil:</label>
+                <input
+                  type="text"
+                  id="maritalstatus"
+                  value={maritalstatus}
+                  onChange={(e) => setMaritalStatus(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[269px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/3">
+                <label htmlFor="crm">CRM:</label>
+                <input
+                  type="text"
+                  id="crm"
+                  value={crm}
+                  onChange={(e) => setCrm(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[223px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/3">
+                <label htmlFor="crm">CBO:</label>
+                <input
+                  type="text"
+                  id="cbo"
+                  value={cbo}
+                  onChange={(e) => setCbo(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[195px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/3">
+                <label htmlFor="specialty">Especialidade:</label>
+                <input
+                  type="text"
+                  id="specialty"
+                  value={specialty}
+                  onChange={(e) => setSpecialty(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[269px]"
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/1">
+                <label htmlFor="specialty">Prestador ativo?:</label>
+                <input
+                  type="radio"
+                  id="activity"
+                  value={activity}
+                  onChange={(e) => setActivity(e.target.value)}
                   className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
                 />
+                <span>Sim</span>
+                <input
+                  type="radio"
+                  id="activity"
+                  value={activity}
+                  onChange={(e) => setActivity(e.target.value)}
+                  className="border-black border-solid border-2 rounded px-1 py-1 w-[191px]"
+                />
+                <span>Não</span>
               </div>
 
               <button
-                className="flex flex-col md:w-1/5"
+                className="flex flex-col md:w-1/1 bg-gradient-to-b from-cyan-600 to-blue-800"
                 type="button"
                 onClick={closeModal}
               >
                 <br />
-                Fechar
+                Adicionar
               </button>
             </form>
           </div>
